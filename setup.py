2a65218e91af485f4fa46894eae13e530ee9eb6d
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 11:08:35 +0000

    Copy test file and README on installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import shutil
+shutil.copy('README.md', 'tisutil/README.md')
 
 setup(name='tisutil',
-      version='0.3',
+      version='0.4',
       description='Utilities related to the scattering quantities',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['tisutil'],
+      package_data={'tisutil': ['tests/*', 'README.md'],
      )
